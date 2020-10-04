13b97ca01c0ae01caf765bb36c6f9119ebc8773d
ShaunWard <shaunalwynward@googlemail.com>
2020-10-04 18:07:47 +0100

    Added a function to compare the value selected by the user with the value of the other card

--- script.js
+++ script.js
@@ -3,38 +3,49 @@ document.getElementById('statTwoPlayer').addEventListener('click', checkComputer
 document.getElementById('statThreePlayer').addEventListener('click', checkComputerStatThree);
 document.getElementById('statFourPlayer').addEventListener('click', checkComputerStatFour);
 
-let playerCount = '0';
-let computerCount = '0';
+let playerCount = 0;
+let computerCount = 0;
 
 
 function startGame(){
     getCardData();
     document.getElementById('gameStart').classList.remove('show');
     document.getElementById('playerCard').classList.add('flip');
+    setScore();
+}
+
+function setScore(){
+    playerCount = 0;
+    computerCount = 0;
     document.getElementById('scorePlayer').innerHTML = 'Score: '+playerCount;
-    document.getElementById('scoreComputer').innerHTML = 'Score: '+playerCount;
+    document.getElementById('scoreComputer').innerHTML = 'Score: '+computerCount;
 }
 
 function updateScore(){
-    document.getElementById('scorePlayer').innerHTML(`<p>Will this work</p>`);
+    document.getElementById('scorePlayer').innerHTML = 'Score: '+playerCount;
+    document.getElementById('scoreComputer').innerHTML = 'Score: '+computerCount;
+    // If score reaches a level end the game and declare winner
 }
 
 function checkComputerStatTwo(){
-    document.getElementById('statTwoPlayer').classList.add('playerSelected');
+    document.getElementById('statTwoPlayer').classList.add('playerSelectedPlayer');
     document.getElementById('computerCard').classList.add('flip');
-    document.getElementById('statTwoComputer').classList.add('playerSelected');
+    document.getElementById('statTwoComputer').classList.add('playerSelectedComputer');
+    compareStat();
 }
 
 function checkComputerStatThree(){
-    document.getElementById('statThreePlayer').classList.add('playerSelected');
+    document.getElementById('statThreePlayer').classList.add('playerSelectedPlayer');
     document.getElementById('computerCard').classList.add('flip');
-    document.getElementById('statThreeComputer').classList.add('playerSelected');
+    document.getElementById('statThreeComputer').classList.add('playerSelectedComputer');
+    compareStat();
 }
 
 function checkComputerStatFour(){
-    document.getElementById('statFourPlayer').classList.add('playerSelected');
+    document.getElementById('statFourPlayer').classList.add('playerSelectedPlayer');
     document.getElementById('computerCard').classList.add('flip');
-    document.getElementById('statFourComputer').classList.add('playerSelected');
+    document.getElementById('statFourComputer').classList.add('playerSelectedComputer');
+    compareStat();
 }
 
 function getCardData(){
@@ -46,14 +57,49 @@ function getCardData(){
         if(this.status == 200){
             let data = JSON.parse(this.responseText);
             console.log(data);
+
+            function randomNumber(data){
+                return data[Math.floor(Math.random() * 510)];
+            }
+
+            let randomPlayer = randomNumber(data);
             
-            document.getElementById('statOnePlayer').innerHTML = `Name: ${data[0].DraftKingsName}`;
-            document.getElementById('statTwoPlayer').innerHTML = `Height: ${data[0].Height}`;
-            document.getElementById('statThreePlayer').innerHTML = `Weight: ${data[0].Weight}`;
-            document.getElementById('statFourPlayer').innerHTML = `Experience: ${data[0].Experience}`;
-            document.getElementById('imagePlayer').src = `${data[0].PhotoUrl}`;
+            console.log(randomPlayer);
+
+            let playerHeight = `${randomPlayer.Height}`;
+            let playerWeight = `${randomPlayer.Weight}`;
+            let playerExperience = `${randomPlayer.Experience}`;
+            
+            document.getElementById('statOnePlayer').innerHTML = `Name: ${randomPlayer.DraftKingsName}`;
+            document.getElementById('statTwoPlayer').innerHTML = `Height: ${playerHeight} Inches`;
+            document.getElementById('statThreePlayer').innerHTML = `Weight: ${playerWeight} Lbs`;
+            document.getElementById('statFourPlayer').innerHTML = `Experience: ${playerExperience} Years`;
+            document.getElementById('imagePlayer').src = `${randomPlayer.PhotoUrl}`;
+
+            let randomComputer = randomNumber(data);
+            let computerHeight = `${randomComputer.Height}`;
+            let computerWeight = `${randomComputer.Weight}`;
+            let computerExperience = `${randomComputer.Experience}`;
+
+            document.getElementById('statOneComputer').innerHTML = `Name: ${randomComputer.DraftKingsName}`;
+            document.getElementById('statTwoComputer').innerHTML = `Height: ${computerHeight} Inches`;
+            document.getElementById('statThreeComputer').innerHTML = `Weight: ${computerWeight} Lbs`;
+            document.getElementById('statFourComputer').innerHTML = `Experience: ${computerExperience} Years`;
+            document.getElementById('imageComputer').src = `${randomComputer.PhotoUrl}`;
         }
         
     }
     xhr.send();
+}
+
+function compareStat(){
+    let valuePlayer = document.getElementsByClassName('playerSelectedPlayer').value;
+    let valueComputer = document.getElementsByClassName('playerSelectedComputer').value;
+
+    if(valuePlayer > valueComputer){
+        playerCount += 1
+    } else {
+        computerCount += 1
+    }
+    updateScore();
 }
